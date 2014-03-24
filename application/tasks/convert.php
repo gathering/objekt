9d2bfc10ddf9bfb6d753f4cc620c2c370ac8e9bb
Simen A.W. Olsen <cobraz@cobraz.no>
2014-03-24 11:50:53 +0100

    Fixing convert-functions.

--- application/tasks/convert.php
+++ application/tasks/convert.php
@@ -8,23 +8,17 @@ class Convert_Task {
 		$files = Fil3::where("type", "=", "map")->where("converted", "=", "0")->get();
 
 		foreach($files as $file){
-			$fp_pdf = fopen($file->url, 'r');
 
-			$img = new imagick('[0]'); // [0] can be used to set page number
-			$img->readImageFile($fp_pdf);
-			$img->setImageFormat( "jpg" );
-			$img->setImageCompression(imagick::COMPRESSION_JPEG); 
-			$img->setImageCompressionQuality(90);
-			$img->setResolution(1200,1200);
-			$img->setImageUnits(imagick::RESOLUTION_PIXELSPERINCH);
-			$img->writeImage('/tmp/map.jpg');
+			system("convert -density 300 {$file->url} /tmp/map-{$file->event_id}.jpg");
+			if(!is_file('/tmp/map.jpg')) continue;
 
-			$event = Config::get('application.event');
 			$filepath = $event->s3_slug."/map.jpg";
 
-			S3::putObject(S3::inputFile('/tmp/map.jpg', false), "s3.obj.no", $filepath, S3::ACL_PUBLIC_READ);
+			S3::putObject(S3::inputFile("/tmp/map-{$file->event_id}.jpg", false), "s3.obj.no", $filepath, S3::ACL_PUBLIC_READ);
+			unlink("/tmp/map-{$file->event_id}.jpg");
 
-			unlink('/tmp/map.jpg');
+			$file->converted = '1';
+			$file->save();
 		}
 
 		return true;
