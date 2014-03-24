509d07a295167b1718a1c6a9918b3f5fa08f29db
Simen A.W. Olsen <cobraz@cobraz.no>
2014-03-24 14:27:07 +0100

    Replace imagick with gs (CLI)

--- application/tasks/convert.php
+++ application/tasks/convert.php
@@ -9,14 +9,17 @@ class Convert_Task {
 
 		foreach($files as $file){
 			$event = $file->event()->first();
-
-			system("convert -density 300 {$file->url} /tmp/map-{$file->event_id}.jpg");
+			$tempPDF = "/tmp/map-{$file->event_id}.pdf";
+			file_put_contents($tempPDF, file_get_contents($file->url));
+			#system("convert -density 300 {$file->url} /tmp/map-{$file->event_id}.jpg");/tmp/map.jpg
+			system("gs -dNumRenderingThreads=4 -dNOPAUSE -sDEVICE=jpeg -dFirstPage=1 -dLastPage=1 -sOutputFile=/tmp/map-{$file->event_id}.jpg -dJPEGQ=100 -r300 -q {$tempPDF} -c quit")
 			if(!is_file('/tmp/map.jpg')) continue;
 
 			$filepath = $event->s3_slug."/map.jpg";
 
 			S3::putObject(S3::inputFile("/tmp/map-{$file->event_id}.jpg", false), "s3.obj.no", $filepath, S3::ACL_PUBLIC_READ);
 			unlink("/tmp/map-{$file->event_id}.jpg");
+			unlink("/tmp/map-{$file->event_id}.pdf");
 
 			$file->childs()->delete();
 
