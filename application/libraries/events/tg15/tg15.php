6c7391b0061a02c747ef7a5f8584f618654717d0
Simen A.W. Olsen <cobraz@cobraz.no>
2015-01-08 10:19:53 +0100

    The Gathering 2015-carepack

---
+++ application/libraries/events/tg15/tg15.php
@@ -0,0 +1,156 @@
+<?php
+/*
+ * The Gathering 2014 special class.
+ *
+ */
+
+include("wannabe.php");
+
+class tg15 extends EventTemplate {
+	var $tag = "wb";
+	function invite(){
+		if(!Input::get())
+			return $this->view('invite.blade.php');
+
+		$username = Input::get('username');
+		$password = Input::get('password');
+
+		$user = Wannabe::auth($username, $password);
+		if(isset($user->error))
+			return Redirect::to('/invite')->with('error', __('common.login_errors.usernotfound'));
+
+		if(!is_array($user->user->crews->crew))
+			return Redirect::to('/invite')->with('error', "You are not apart of any crew, so we cannot add your account.");
+
+		// Special users
+		switch($user->user->id){
+			case 5497:
+				$role = Role::find(6);
+			break;
+			case 2260:
+				$role = Role::find(8);
+			break;
+			case 3704:
+				$role = Role::find(11);
+			break;
+			default: case "":
+				foreach($user->user->crews->crew as $crew){
+					switch($crew->id){
+						case 279:
+							$role = Role::findByName($crew->name)->first();
+						break;
+						case 275:
+							$role = Role::findByName($crew->name)->first();
+						break;
+						case 278:
+							$role = Role::findByName($crew->name)->first();
+						break;
+					}
+				}
+			break;
+		}
+
+		if(!$role)
+			return Redirect::to('/invite')->with('error', "Not apart of a crew that are eligible to have an Objekt-account.");
+
+		$objUser = User::where("username", "=", $user->user->username);
+		if($objUser->count() > 0){
+			$objUser = $objUser->where("email", "=", $user->user->email);
+			if($objUser = $objUser->first()){
+				if($objUser->events()->where('events.id', '=', 2)->first())
+					return Redirect::to('/invite')->with('error', "You are already registred with this event.");
+			} else return Redirect::to('/invite')->with('error', "This username already exists, but with another email. We don't know it's you, so.. I'm sorry?");
+		} else $objUser = new Verify\Models\User;
+		
+		$objUser->username = @$user->user->username;
+		$objUser->password = $password;
+		$objUser->email = @$user->user->email;
+		$objUser->name = empty($user->user->realname) ? $user->user->username : $user->user->realname;
+		$objUser->verified = 1;
+		$objUser->profile_img = isset($user->user->images->image[3]->url) ? $user->user->images->image[3]->url : "";
+		$objUser->meta = serialize($user);
+		$objUser->save();
+
+		$objUser->roles()->sync(array($role->id));
+
+		return Redirect::to('/login')->with('success', 'Your account has been created.');
+
+		// Crew_ID : SponsorSupport : 279
+		// Crew_ID : Info Desk : 275
+	}
+
+	function synchronizeUsers(){
+		// Create Index
+		#$indexParams['index']  = 'wannabe_tg14';
+		#Elastisk::indices()->create($indexParams);
+
+		$currentUser = Auth::user();
+		if(isset($currentUser->meta()->apikey)){
+			$apikey = $currentUser->meta()->apikey;
+		} else $apikey = "1d7fcf1944cbc683ea6bc3c044c9c2cd";
+
+		Wannabe::setAPIKey($apikey);
+		
+		$i=0;
+		foreach(Wannabe::users()->users->user as $user){
+			$params = array();
+			$params['body']  = array(
+				'name' => $user->realname,
+				'id' => $user->id
+				);
+
+			$params['index'] = 'wannabe_tg14';
+			$params['type']  = 'user';
+			$params['id']    = $user->id;
+
+			Elastisk::index($params);
+			$i++;
+		}
+
+		return $i." users added.";
+
+	}
+
+	function searchUsers($term){
+
+		$params['index'] = 'wannabe_tg15';
+		$params['type']  = 'user';
+		$params['body']['query']['query_string']['query'] = $term;
+		if($term == "*") $params['body']['size'] = 10000;
+
+		$results = array();
+		$elastisk = Elastisk::search($params);
+
+		foreach($elastisk['hits']['hits'] as $result){
+			$person = array();
+			$person['name'] = "Wannabe: ".$result['_source']['name'];
+			$person['value'] = "wb:".$result['_id'];
+			array_push($results, $person);
+		}
+
+		return $results;
+
+	}
+
+	function getUser($id){
+		
+		$id = intval($id);
+
+		$currentUser = Auth::user();
+		
+		if(isset($currentUser->meta()->apikey)){
+			$apikey = $currentUser->meta()->apikey;
+		} else $apikey = "1d7fcf1944cbc683ea6bc3c044c9c2cd";
+
+		Wannabe::setAPIKey($apikey);
+		$user = Wannabe::user($id);
+
+		if(isset($user->user)){
+			$user->user->name = $user->user->realname; // Objekt Fix.
+			return $user->user;
+		} else
+			return array();
+	}
+}
+
+?>
---
+++ application/libraries/events/tg15/wannabe.php
@@ -0,0 +1,59 @@
+<?php
+
+
+class Wannabe {
+
+	static private $app_key = "53299290-9920-4cba-9afb-433bc39fe93c";
+	static private $api_url = "http://wannabe.gathering.org/tg14/api/";
+
+	static private $api_key = "";
+
+	static private $query_string = array();
+
+	static function auth($username, $password){
+		self::setQuery("username", $username);
+		self::setQuery("password", md5($password));
+		return self::run('auth');
+	}
+
+	static function setAPIKey($api_key){
+		self::$api_key = $api_key;
+	}
+
+	static function user($id){
+		self::setQuery("apikey", self::$api_key);
+		return self::run('users/view/'.$id);
+	}
+
+	static function users(){
+		self::setQuery("apikey", self::$api_key);
+		return self::run('users');
+	}
+
+	private static function setQuery($name, $value){
+		self::$query_string[$name] = $value;
+	}
+
+	private static function getQuery(){
+		self::$query_string['app'] = self::$app_key;
+		return "?".http_build_query(self::$query_string);
+	}
+
+	private static function run($method=""){
+
+		$url = self::$api_url.$method.self::getQuery();
+
+		$ch = curl_init($url);
+	    curl_setopt($ch, CURLOPT_HEADER, 0);
+	    curl_setopt($ch, CURLOPT_TIMEOUT, 4);
+	    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 2);
+	    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+	    $return = curl_exec($ch);
+	    return json_decode($return) ? json_decode($return) : $return;
+	}
+
+}
+
+?>
