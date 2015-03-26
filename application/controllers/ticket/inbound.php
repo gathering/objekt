7e51e13f5af7d7338842695fbf9a1a1bbefd5768
Simen A.W. Olsen <cobraz@cobraz.no>
2015-03-26 08:51:19 +0100

    Fixing inbound handler.

--- application/controllers/ticket/inbound.php
+++ application/controllers/ticket/inbound.php
@@ -14,16 +14,18 @@ class Ticket_Inbound_Controller extends Base_Controller {
 		$events = json_decode( Input::get('mandrill_events') );
 		if(!$events) Response::json(array('error' => 'Not correct format applied'), 505);
 
+		var_dump($events); exit;
+
 		foreach($events as $event){
 			$message = new Message;
 
-			$message->from_email = $event->from_email;
-			$message->to_email = $event->email;
-			$message->subject = $event->subject;
-			$message->tags = json_encode($event->tags);
-			$message->raw_message = $event->raw_msg;
-			$message->html = $event->html;
-			$message->text = $event->text;
+			$message->from_email = $event->msg->from_email;
+			$message->to_email = $event->msg->email;
+			$message->subject = $event->msg->subject;
+			$message->tags = json_encode($event->msg->tags);
+			$message->raw_message = $event->msg->raw_msg;
+			$message->html = $event->msg->html;
+			$message->text = $event->msg->text;
 
 			$message->save();
 		}
